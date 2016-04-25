d79d941d6850b86b21b74acb69140db3dbf41e98
Krasimir Tsonev <krasimir@outset.ws>
2016-04-25 15:15:43 +0300

    Addinga test for deep nesting

---
+++ test/cssx/nested/50/actual.js
@@ -0,0 +1,19 @@
+cssx(
+  .container {
+    font-size: 20px;
+    .nav {
+      a.link {
+        color: #FFF;
+      }
+      banner {
+        width: 100%;
+        small {
+          font-size: 0.2em;
+        }
+      }
+    }
+    section.content {
+      height: 20px;
+    }
+  }
+);
